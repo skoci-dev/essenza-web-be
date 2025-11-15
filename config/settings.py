5f9ab470e157b3744e8d27faa852955752a75e54
ruriazz <azizruri.s@gmail.com>
2025-11-16 02:06:56 +0700

    fix: Update default database name for development environment

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME", "essenza"),
+        "NAME": os.environ.get("DB_NAME", "essenza_db_dev"),
         "USER": os.environ.get("DB_USER", "root"),
         "PASSWORD": os.environ.get("DB_PASSWORD", "root"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
