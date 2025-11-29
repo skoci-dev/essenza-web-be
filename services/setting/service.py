013292b4895bb24fe016b4a6209ac859297873e9
ruriazz <azizruri.s@gmail.com>
2025-11-29 22:25:40 +0700

    feat(settings): Enhance SettingService with context management and logging for create, update, and delete operations

--- services/setting/service.py
+++ services/setting/service.py
@@ -1,9 +1,11 @@
+import copy
 from typing import Tuple
 from django.db.models.manager import BaseManager
 from django.utils.text import slugify
 
-from core.service import BaseService
+from core.service import BaseService, required_context
 from core.models import Setting
+from core.enums import ActionType
 
 
 class SettingService(BaseService):
@@ -11,6 +13,7 @@ class SettingService(BaseService):
     Service class for managing application settings
     """
 
+    @required_context
     def create_setting(self, **setting_data) -> Tuple[Setting, Exception | None]:
         """
         Create a new application settings
@@ -21,7 +24,14 @@ class SettingService(BaseService):
         if Setting.objects.filter(slug=slug).exists():
             return Setting(), Exception(f"Setting with slug '{slug}' already exists.")
 
-        return Setting.objects.create(**setting_data), None
+        setting = Setting.objects.create(**setting_data), None
+        self.log_entity_change(
+            self.ctx,
+            action=ActionType.CREATE,
+            instance=setting[0],
+            description=f"Created setting '{setting[0].label}'.",
+        )
+        return setting
 
     def get_all_settings(self) -> BaseManager[Setting]:
         """
@@ -38,6 +48,7 @@ class SettingService(BaseService):
         except Setting.DoesNotExist:
             return None
 
+    @required_context
     def update_setting_by_slug(
         self, slug: str, **update_data
     ) -> Tuple[Setting, Exception | None]:
@@ -48,11 +59,20 @@ class SettingService(BaseService):
         if not setting:
             return Setting(), Exception(f"Setting with slug '{slug}' does not exist.")
 
+        old_setting = copy.deepcopy(setting)
         for key, value in update_data.items():
             setattr(setting, key, value)
         setting.save()
+        self.log_entity_change(
+            self.ctx,
+            action=ActionType.UPDATE,
+            instance=setting,
+            old_instance=old_setting,
+            description=f"Updated setting '{setting.label}'.",
+        )
         return setting, None
 
+    @required_context
     def delete_setting_by_slug(self, slug: str) -> Exception | None:
         """
         Delete a specific setting by its slug
@@ -61,5 +81,12 @@ class SettingService(BaseService):
         if not setting:
             return Exception(f"Setting with slug '{slug}' does not exist.")
 
+        old_setting = copy.deepcopy(setting)
         setting.delete()
+        self.log_entity_change(
+            self.ctx,
+            action=ActionType.DELETE,
+            instance=old_setting,
+            description=f"Deleted setting '{old_setting.label}'.",
+        )
         return None
